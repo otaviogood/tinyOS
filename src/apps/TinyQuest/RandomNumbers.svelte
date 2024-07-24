6999ab27c75a8b35afae28089c45409ac1ecaaa8
Otavio Good <otaviogood@yahoo.com>
2024-07-24 07:43:16 -0700

    cleaned up random numbers game

--- src/apps/TinyQuest/RandomNumbers.svelte
+++ src/apps/TinyQuest/RandomNumbers.svelte
@@ -14,6 +14,7 @@
     import { sleep, getRandomInt, preventZoom } from "./util";
     import { pulseShadow, scaleDown, scalePulse } from "./Transitions";
     import { speechPlay } from "../../utils";
+    import Line from "../../components/Line.svelte";
 
     var snd_good = new Howl({ src: ["/TinyQuest/sfx/sfx_coin_double1.wav"], volume: 0.25 });
     var snd_fanfare = new Howl({ src: ["/TinyQuest/sfx/sfx_sound_mechanicalnoise2.wav"], volume: 0.25 });
@@ -33,28 +34,39 @@
     let numOptions = 3; // Increased by 1 to include the target number
     let difficulty = 1; // Number of digits in the numbers
 
+    function genPair(sx, sy, ox, oy) {
+        let rad = 0.28;
+        let x = Math.random() * sx + ox;
+        let y = Math.random() * sy + oy;
+        // let x = Math.random() * sx - sx*.5;
+        // let y = Math.random() * sy - sy*.5;
+        // // normalize all points to lie on a circle, with radius .4
+        // let r = Math.sqrt(x * x + y * y);
+        // x = x / r * rad + sx*0.5;
+        // y = y / r * rad + sy*0.5;
+        // x += ox;
+        // y += oy;
+        return [x, y];
+    }
     function genPos() {
         let sx = 0.71;
         let sy = 0.55;
         let ox = 0.02;
         let oy = 0.02;
-        let x = Math.random() * sx + ox;
-        let y = Math.random() * sy + oy;
-        
+        let [x, y] = genPair(sx, sy, ox, oy);
+
         for (let reps = 0; reps < 100; reps++) {
             let collision = false;
             if ((x < 0.2 && y < 0.2) || (x < 0.2 && y > 0.4) || (Math.abs(x - 0.5) < 0.2 && Math.abs(y - 0.5) < 0.2)) {
                 collision = true;
-                x = Math.random() * sx + ox;
-                y = Math.random() * sy + oy;
+                [x, y] = genPair(sx, sy, ox, oy);
             }
             for (let i = 0; i < numberOptions?.length || 0; i++) {
                 let dx = x - numberOptions[i].x;
                 let dy = y - numberOptions[i].y;
                 if (Math.sqrt(dx * dx + dy * dy) < 0.16) {
                     collision = true;
-                    x = Math.random() * sx + ox;
-                    y = Math.random() * sy + oy;
+                    [x, y] = genPair(sx, sy, ox, oy);
                 }
             }
             if (!collision) break;
@@ -70,6 +82,10 @@
 
         for (let i = 1; i < numOptions; i++) {
             let num = getRandomInt(Math.pow(10, difficulty) - 1);
+            // If duplicate, try again
+            while (numberOptions.find(option => option.number === num)) {
+                num = getRandomInt(Math.pow(10, difficulty) - 1);
+            }
             let [x, y] = genPos();
             numberOptions.push({ number: num, x, y, clicked: false, isTarget: false });
         }
@@ -139,42 +155,44 @@
     loop();
 </script>
 
-<div class="fit-full-space select-none overflow-hidden" style="background-color:#6fd0c0" on:touchstart={preventZoom}>
+<div class="fit-full-space select-none overflow-hidden" style="background-color:#f6f8f8" on:touchstart={preventZoom}>
     <div class="relative overflow-hidden select-none" style="width:{$bigWidth}; height:{$bigHeight};margin-left:{$bigPadX}px;margin-top:{$bigPadY}px">
         {#if !started}
             <div class="flex-center-all h-full flex flex-col">
-                <!-- <img src="TinyQuest/gamedata/numberpies/wood.webp" class="fit-full-space" alt="wood background" style="" /> -->
-                <div class="absolute top-0 left-0 w-full h-full" style="background-color:#6fd0c0b0" ></div>
-                <div class="text-9xl font-bold m-8 z-10">{town?.name}</div>
+                <img src="TinyQuest/gamedata/randomnumbers/swirls.webp" class="fit-full-space" alt="wood background" style="" />
+                <!-- <div class="absolute top-0 left-0 w-full h-full" style="background-color:#6fd0c020" ></div> -->
+                <div class="text-9xl font-bold m-8 z-10 rounded-2xl text-teal-800 bg-teXal-300/30 p-6">{town?.name}</div>
                 <!-- Difficulty buttons 1-4 -->
-                <div class="text-9xl font-bold m-8 z-10 rounded-2xl border-2 border-gray-500 bg-green-200 text-black">Difficulty<br/>
-                {#each Array(4) as _, i}
-                    <button
-                        class="bg-blue-500 text-white text-9xl rounded-3xl px-8 z-10"
-                        style="margin:1rem"
-                        on:pointerup={() => {difficulty = i + 1; numOptions = 3; startGame()}}
-                    >
-                        {i + 1}
-                    </button>
-                {/each}
-            </div>
+                <div class="text-9xl font-bold m-8 z-10 rounded-2xl text-teal-800 bg-teal-300/30 p-6">&nbsp;Difficulty<br/>
+                    {#each Array(4) as _, i}
+                        <button
+                            class="bg-blue-500 text-white text-9xl rounded-3xl px-8 z-10"
+                            style="margin:1rem"
+                            on:pointerup={() => {difficulty = i + 1; numOptions = 3; startGame()}}
+                        >
+                            {i + 1}
+                        </button>
+                    {/each}
+                </div>
                 <!-- <button class="bg-red-500 text-white text-9xl rounded-3xl px-8 z-10" on:pointerup|preventDefault|stopPropagation={startGame}>START</button> -->
             </div>
         {:else}
         <div class="flex flex-row h-full w-full">
-            <!-- <img src="TinyQuest/gamedata/numberpies/wood.webp" class="absolute top-0 left-0 w-full h-full" alt="wood background" style="" /> -->
-            <div class="absolute top-0 left-0 w-full h-full" style="background-color:#6fd0c0b0" ></div>
+            <img src="TinyQuest/gamedata/randomnumbers/swirls.webp" class="absolute top-0 left-0 w-full h-full" alt="wood background" style="" />
+            <!-- <div class="absolute top-0 left-0 w-full h-full" style="background-color:#6fd0c020" ></div> -->
+            <!-- <div class="absolute rounded-full border-2 w-[72rem] h-[60rem] border-gray-600"></div> -->
+            <!-- <Line x0={43.5} y0={37.5} thick={41} style="border:2px solid black" rounded color="#0000d000" /> -->
             <div id="playBoard" class="h-full w-full font-bold relative">
                 {#if numberOptions}
                     {#each numberOptions as option, i}
-                        <div
-                            class="absolute text-4xl flex-center-all rounded-full active:scale-105 transform transition-all duration-75"
-                            style="width:15rem;height:15rem;left:{option.x * 100}rem;top:{option.y * 100}rem;font-size:6rem;color:#ffffff;line-height:3rem;background-color:{option.isTarget ? '#bf3434' : option.clicked ? '#4299e1' : '#707e90'};"
-                            on:pointerup|preventDefault|stopPropagation={() => clickNumber(option)}
-                        >
-                            {option.number}
-                        </div>
-                    {/each}
+                    <div in:fade
+                    class="absolute text-4xl flex-center-all rounded-full active:scale-105 transform transition-all duration-1000 cursor-pointer"
+                    style="width:15rem;height:15rem;left:{option.x * 100}rem;top:{option.y * 100}rem;font-size:6rem;color:#ffffff;line-height:3rem;background-color:{option.isTarget ? '#bf3474d0' : option.clicked ? '#4299e1d0' : '#707e90d0'};"
+                    on:pointerup|preventDefault|stopPropagation={() => clickNumber(option)}
+                >
+                    {option.number}
+                </div>
+            {/each}
                 {/if}
             </div>
             <StarBar {maxStars} {starCount} bg="#ffffff" on:pointerup={resetToSplashScreen} />
